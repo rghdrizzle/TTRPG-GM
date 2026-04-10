9b85c0c205b9cb97030a4ba8d7306e14dd003930
Luqmaan Hakeem <rockgameplayhakeem@gmail.com>
2026-04-10 11:56:24 +0530

    code: initialized frontend

--- app/src/app/main.py
+++ app/src/app/main.py
@@ -3,6 +3,7 @@ from app.routers import routes
 
 app = FastAPI()
 app.include_router(routes.router)
+app.include_router(routes.protected_router)
 
 @app.get("/") # decorator function
 async def root(): # async here is for coroutine
