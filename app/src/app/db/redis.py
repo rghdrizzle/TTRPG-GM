34d983c7e53d4547578691f945025a2acbdecd62
Luqmaan Hakeem <rockgameplayhakeem@gmail.com>
2026-06-11 11:49:12 +0530

    code: added socket manager

---
+++ app/src/app/db/redis.py
@@ -0,0 +1,35 @@
+import asyncio
+import redis.asyncio as aioredis
+import json
+from fastapi import WebSocket
+
+
+class RedisPubSubManager:
+    """
+    Creates an instance of this class which manages Redis connection and implement Pub and Sub pattern
+    args: host(str) port(int)
+    """
+
+    def __init__(self,host="localhost",port=6369):
+        self.redis_host = host
+        self.redis_port = port
+        self.pubsub = None
+
+    async def __get_redis_connection(self) -> aioredis:
+        return aioredis.Redis(host=self.redis_host,port=self.redis_port,auto_close_connection_pool=False)
+    
+    async def connect(self)->None:
+        self.redis_connection = self.__get_redis_connection()
+        self.pubsub = self.redis_connection.pubsub()
+
+    async def _publish(self,room_id:str, message:str)-> None:
+        await self.redis_connection.publish(room_id,message)
+    
+    async def subscribe(self, room_id: str)-> aioredis.Redis:
+        await self.pubsub.subscribe(room_id)
+        return self.pubsub
+    async def unsubscribe(self, room_id)->None:
+        await self.pubsub.unsubscribe(room_id)
+        
+
+    
